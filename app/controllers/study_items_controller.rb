4c108d886091837da00110310775edcacd34df21
leandrohorizon <leandro.mcontato@gmail.com>
2021-03-10 18:55:53 -0300

    insert data

--- app/controllers/study_items_controller.rb
+++ app/controllers/study_items_controller.rb
@@ -1,6 +1,27 @@
 class StudyItemsController < ApplicationController
   def show
-    @study_item = Study.find(params[:id]) #id n precisa ser find_by
+    @study_item = StudyItem.find(params[:id]) #id n precisa ser find_by
     #@study_item = Study.find_by(id: params[:id])
+  end
+
+  def new
+    @study_item = StudyItem.new
+  end
+
+  def create
+    si_params = params.require(:study_item).permit(:title, :category, :done)
+    @study_item = StudyItem.new(si_params)
+
+    # @study_item.title = params[:study_item][:title]
+    # @study_item.category = params[:study_item][:category]
+
+    if @study_item.save
+      flash[:notice] = "Novo item adionado"
+      redirect_to root_path
+    else
+      # redirect_to new_study_item_path
+      flash[:alert] = "Não foi possível adicionar o item de estudo"
+      render "new"
+    end
   end
 end
--- app/models/study_item.rb
+++ app/models/study_item.rb
@@ -1,5 +1,6 @@
 class StudyItem < ApplicationRecord
-
+  validates :title, presence: true
+  
   def full_title
     "#{title} - #{category}"
   end
