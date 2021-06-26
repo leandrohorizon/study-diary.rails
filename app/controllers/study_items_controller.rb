77fecb5d9a5caeac0f2a6500fd6032299e376b10
leandrohorizon <leandro.mcontato@gmail.com>
2021-06-26 11:05:13 -0300

    Refatora controller de study items

--- app/controllers/study_items_controller.rb
+++ app/controllers/study_items_controller.rb
@@ -1,7 +1,6 @@
 class StudyItemsController < ApplicationController
+  before_action :set_study_item, only: [:show, :edit, :update, :mark_as_done]
   def show
-    @study_item = StudyItem.find(params[:id]) #id n precisa ser find_by
-    #@study_item = Study.find_by(id: params[:id])
   end
 
   def new
@@ -11,42 +10,38 @@ class StudyItemsController < ApplicationController
   def create
     @study_item = StudyItem.new(study_item_params)
 
-    # @study_item.title = params[:study_item][:title]
-    # @study_item.category = params[:study_item][:category]
-
     if @study_item.save
-      flash[:notice] = "Novo item adionado"
+      flash[:notice] = 'Novo item adionado'
       redirect_to root_path
     else
-      # redirect_to new_study_item_path
-      flash[:alert] = "Não foi possível adicionar o item de estudo"
-      render "new"
+      flash[:alert] = 'Não foi possível adicionar o item de estudo'
+      render 'new'
     end
   end
 
   def edit
-    set_study_item
   end
 
   def update
-    @study_item = StudyItem.find(params[:id])
-    if @study_item.update(study_item_params)
-      redirect_to study_item_path(@study_item)
-    end
+    redirect_to study_item_path(@study_item) if @study_item.update(study_item_params)
   end
 
   def mark_as_done
-    set_study_item
     @study_item.done!
     redirect_to @study_item
   end
 
-  private
-    def set_study_item
-      @study_item = StudyItem.find(params[:id])
-    end
+  def hello_world
+    render json: { message: 'Hello World' }
+  end
 
-    def study_item_params
-      params.require(:study_item).permit(:title, :category_id, :done)
-    end
+  private
+
+  def set_study_item
+    @study_item = StudyItem.find(params[:id])
+  end
+
+  def study_item_params
+    params.require(:study_item).permit(:title, :category_id, :done)
+  end
 end
